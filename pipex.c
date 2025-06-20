9a61fdb84700129865097f48dd3ab28ae30800bf
Daniel Jiménez <danielji@student.42madrid.com>
2025-06-20 12:29:23 +0200

    comments

--- pipex.c
+++ pipex.c
@@ -6,19 +6,18 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 10:32:16 by danielji          #+#    #+#             */
-/*   Updated: 2025/06/20 11:47:28 by danielji         ###   ########.fr       */
+/*   Updated: 2025/06/20 12:01:33 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "pipex.h"
 /* PRUEBAS:
 < Makefile grep libft | grep clean > outfile
-grep libft < Makefile | grep clean > outfile
 
 < Makefile grep libft | tr -d "@" > outfile
-grep libft < Makefile | tr -d "@" > outfile
+./pipex Makefile "grep libft" "tr -d @" outfile
 */
-int	main(int argc, char *argv[])
+int	main(int argc, char *argv[], char *envp[])
 {
 	if (argc != 5)
 		return (0);
@@ -31,12 +30,12 @@ int	main(int argc, char *argv[])
 	filedes[1] = open(argv[4], O_RDWR);
 
 	pipe(filedes);
-	command1 = ft_split(argv[2], ' ');
+/* 	command1 = ft_split(argv[2], ' ');
 	command2 = ft_split(argv[3], ' ');
 
-	//char *pathname = ft_strjoin("/usr/bin/", command1[0]);
-	//execve(pathname, command1, envp);
-	//free(command1);
-	//free(pathname);
+	char *pathname1 = ft_strjoin("/usr/bin/", command1[0]);
+	execve(pathname1, command1, envp);
+	free(command1);
+	free(pathname1); */
 	return (0);
 }
