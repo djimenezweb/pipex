63b068838b4f14366decb6c327692fbd947a2d57
Daniel Jiménez <danielji@student.42madrid.com>
2025-06-23 13:22:14 +0200

    added is_space function

--- split_helpers.c
+++ split_helpers.c
@@ -6,11 +6,18 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 11:52:20 by danielji          #+#    #+#             */
-/*   Updated: 2025/06/23 13:15:23 by danielji         ###   ########.fr       */
+/*   Updated: 2025/06/23 13:20:54 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "pipex.h"
+
+static int	is_space(char c)
+{
+	if (c == ' ')
+		return (1);
+	return (0);
+}
 
 char	*split_command(char *str)
 {
@@ -21,10 +28,10 @@ char	*split_command(char *str)
 
 	i = 0;
 	len = 0;
-	while (str[i] && str[i] == ' ')
+	while (str[i] && is_space(str[i]))
 		i++;
 	start = i;
-	while (str[i] && str[i] != ' ')
+	while (str[i] && !is_space(str[i]))
 	{
 		i++;
 		len++;
@@ -40,14 +47,14 @@ char	*split_args(char *str)
 	char	*args;
 
 	start = 0;
-	while (str[start] && str[start] == ' ')
+	while (str[start] && is_space(str[start]))
 		start++;
-	while (str[start] && str[start] != ' ')
+	while (str[start] && !is_space(str[start]))
 		start++;
-	while (str[start] && str[start] == ' ')
+	while (str[start] && is_space(str[start]))
 		start++;
 	end = ft_strlen(str) - 1;
-	while (str[end] && str[end] == ' ')
+	while (str[end] && is_space(str[end]))
 		end--;
 	args = ft_substr(str, start, (end - start + 1));
 	return (args);
