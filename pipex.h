9c267841eade080e06eb67940c62fa011f6513c0
Daniel Jiménez <danielji@student.42madrid.com>
2025-06-24 12:11:27 +0200

    Path function helpers

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 10:58:43 by danielji          #+#    #+#             */
-/*   Updated: 2025/06/23 12:51:32 by danielji         ###   ########.fr       */
+/*   Updated: 2025/06/24 10:50:35 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -22,5 +22,8 @@
 
 char	*split_command(char *str);
 char	*split_args(char *str);
+
+char	*path_env(char *envp[]);
+char	*path(char *command, char *envp[]);
 
 #endif
